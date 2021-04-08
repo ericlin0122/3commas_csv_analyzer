c250fb54cbb3043c425eaa9022e45f0660c79385
Eric Lin <ericlin0122@gmail.com>
2021-04-08 07:30:16 -0600

    check status

--- 3commas_csv_analyzer.rb
+++ 3commas_csv_analyzer.rb
@@ -33,6 +33,8 @@ File.readlines(file).each do |line|
         items.each_with_index {|value, index| indexes[value] = index }
         next
     end
+    status = items[indexes["status"]]
+    next unless status =~ /completed/i
     bot = items[indexes["bot"]]
     pair = items[indexes["pair"]]
     profit_percentage_from_total_volume = items[indexes["profit_percentage_from_total_volume"]]
@@ -51,14 +53,14 @@ header = "name, deal_count, total_profit_percentage_from_total_volume, total_fin
 
 def print_stat(data, now, hours, comment)
     puts "#{'='*12} #{comment} #{'='*12} "
-    puts "name, deal_count, total_profit_percentage_from_total_volume, total_final_profit, total_used_safety_orders(average per trade)"
+    puts "name, deal_count, total_profit_percentage_from_total_volume(%), total_final_profit($), total_used_safety_orders(average per trade)"
     i = 1
     data.inject({}) { |h, (k, v)| h[k] = v.reject{|hash| now - hours*HOUR >= hash[:closed_at] }; h }.sort_by{|k, v| v.size}.reverse.each do |name, d| 
         next if d.size <= MIN_DEAL_COUNT
         total_profit_percentage_from_total_volume = d.inject(0.0) {|sum, hash| sum + hash[:profit_percentage_from_total_volume]}
         total_used_safety_orders = d.inject(0) {|sum, hash| sum + hash[:used_safety_orders]}
         total_final_profit = d.inject(0) {|sum, hash| sum + hash[:final_profit]}
-        puts "#{i} #{name}, #{d.size}, #{total_profit_percentage_from_total_volume.round(3)}, #{total_final_profit.round(3)}, #{total_used_safety_orders}(#{(total_final_profit/d.size*1.0).round(1)})" 
+        puts "#{i} #{name}, #{d.size}, #{total_profit_percentage_from_total_volume.round(3)}%, $#{total_final_profit.round(3)}, #{total_used_safety_orders}(#{(total_final_profit/d.size*1.0).round(1)})" 
         i += 1
     end
 end
@@ -69,4 +71,5 @@ print_stat(data,now,24 * 7, "last 7 days")
 print_stat(data,now,24, "last 24 hours")
 print_stat(data,now,12, "last 12 hours")
 print_stat(data,now,6, "last 6 hours")
+print_stat(data,now,3, "last 3 hours")
 puts "file processed: #{file}"
