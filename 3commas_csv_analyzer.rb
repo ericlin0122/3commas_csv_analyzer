a551b26fc3b6b9db7d00cb80ebcae44c8eca90d6
Eric Lin <ericlin0122@gmail.com>
2021-04-11 09:24:13 -0600

    add more safety stats

--- 3commas_csv_analyzer.rb
+++ 3commas_csv_analyzer.rb
@@ -37,13 +37,12 @@ File.readlines(file).each do |line|
     status = items[indexes["status"]]
     next unless status =~ /completed/i
     bot = items[indexes["bot"]]
-    next if bot == "complex bot - btc strong buy"
     pair = items[indexes["pair"]]
     profit_percentage_from_total_volume = items[indexes["profit_percentage_from_total_volume"]]
     final_profit = items[indexes["final_profit"]]
     closed_at = items[indexes["closed_at"]]
     used_safety_orders = items[indexes["used_safety_orders"]].to_i
-    bot_type = items[indexes["bot_pairs"]].to_i > 1 ? "complex" : "simple"
+    bot_type = items[indexes["bot_pairs"]].to_i > 1 ? "composite" : "simple"
     name = "#{bot} - #{pair}"
     if data[name]
         data[name] << {pair: pair, profit_percentage_from_total_volume: profit_percentage_from_total_volume.to_f, final_profit: final_profit.to_f, closed_at: Time.parse(closed_at + "UTC"), used_safety_orders: used_safety_orders, bot_type: bot_type}
@@ -58,14 +57,22 @@ def print_stat(data, now, hours, comment)
     puts "#{'='*12} #{comment} #{'='*12} "
     puts "name, deal_count, total_profit_percentage_from_total_volume(%), total_final_profit($), total_used_safety_orders(average per trade)"
     i = 1
-    complex = {pairs: 0, deal_count: 0, profit_in_dollar: 0.0, profit_in_percent: 0.0, safety_orders: 0}
+    composite = {pairs: 0, deal_count: 0, profit_in_dollar: 0.0, profit_in_percent: 0.0, safety_orders: 0}
     simple = {pairs: 0, deal_count: 0, profit_in_dollar: 0.0, profit_in_percent: 0.0, safety_orders: 0}
+    closed_safety_order_stats = {}
     data.inject({}) { |h, (k, v)| h[k] = v.select{|hash| now - hours*HOUR < hash[:closed_at].to_i }; h }.sort_by{|k, v| v.size}.reverse.each do |name, d| 
         next if d.size <= MIN_DEAL_COUNT
         total_profit_percentage_from_total_volume = d.inject(0.0) {|sum, hash| sum + hash[:profit_percentage_from_total_volume]}
         total_used_safety_orders = d.inject(0) {|sum, hash| sum + hash[:used_safety_orders]}
         total_final_profit = d.inject(0) {|sum, hash| sum + hash[:final_profit]}
         bot_type = d.first[:bot_type] 
+        d.each do |trade|
+            if closed_safety_order_stats.include?(trade[:used_safety_orders])
+                closed_safety_order_stats[trade[:used_safety_orders]] += 1
+            else
+                closed_safety_order_stats[trade[:used_safety_orders]] = 1
+            end
+        end
         if bot_type == 'simple'
             simple[:pairs] += 1
             simple[:deal_count] += d.size
@@ -73,20 +80,23 @@ def print_stat(data, now, hours, comment)
             simple[:profit_in_percent] += total_profit_percentage_from_total_volume
             simple[:safety_orders] += total_used_safety_orders
         else
-            complex[:pairs] += 1
-            complex[:deal_count] += d.size
-            complex[:profit_in_dollar] += total_final_profit
-            complex[:profit_in_percent] += total_profit_percentage_from_total_volume
-            complex[:safety_orders] += total_used_safety_orders
+            composite[:pairs] += 1
+            composite[:deal_count] += d.size
+            composite[:profit_in_dollar] += total_final_profit
+            composite[:profit_in_percent] += total_profit_percentage_from_total_volume
+            composite[:safety_orders] += total_used_safety_orders
         end
         ratio = (total_final_profit/d.size*1.0).round(1)
-        puts "#{i} #{bot_type == 'complex' ? name.light_blue : name }, #{d.size}, #{total_profit_percentage_from_total_volume.round(3)}%, $#{total_final_profit.round(3)}, #{total_used_safety_orders}(#{ratio >= 1.0 ? "#{ratio}".red : ratio})" 
+        puts "#{i} #{bot_type == 'composite' ? name.light_blue : name }, #{d.size}, #{total_profit_percentage_from_total_volume.round(3)}%, $#{total_final_profit.round(5)}, #{total_used_safety_orders}(#{ratio >= 1.0 ? "#{ratio}".red : ratio})" 
         i += 1
     end
     puts '-' * 40
     puts "simple pair stats: #{simple.inspect}"
-    puts "complex pair stats: #{complex.inspect}"
-    puts "combined stats: #{simple.merge(complex) { |k, o, n| o + n }}"
+    puts "composite pair stats: #{composite.inspect}"
+    puts "combined stats: #{simple.merge(composite) { |k, o, n| o + n }}"
+    puts "closed_safety_order_stats stats"
+    total_safety = closed_safety_order_stats.inject(0.0) {|sum, (k,v)| sum + v}
+    closed_safety_order_stats.each.sort.each {|key,value| puts "#{key}: #{value} (#{(value / total_safety * 100).round(2)}%)"}
 end
 
 print_stat(data,now,99999, "all time")
@@ -98,4 +108,3 @@ print_stat(data,now,6, "last 6 hours")
 print_stat(data,now,3, "last 3 hours")
 print_stat(data,now,1, "last 1 hours")
 puts "file processed: #{file}"
-puts Time.now
